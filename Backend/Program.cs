8661439f0aa370cc0c10bf75676798ea734a2aba
Kosta <koktashev33@gmail.com>
2023-03-06 22:53:39 +0200

    Add project files.

---
+++ Backend/Program.cs
@@ -0,0 +1,32 @@
+﻿using MessegeCenter;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.RegisterMesseges();
+        }
+
+        private   void RegisterMesseges()
+        {
+            WeakReferenceMessenger.Default.Register<SaveMessege>(this, (r, m) =>
+            {
+                SaveNumber(m);
+            });
+        }
+
+        private static void SaveNumber(object value)
+        {
+            //Save To DB
+        }
+    }
+}
---
+++ Calculator/ViewModels/CalculatorVM.cs
@@ -0,0 +1,56 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using MessegeCenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculator.ViewModels
+{
+    public class CalculatorVM: ObservableObject
+    {
+
+        [ObservableProperty]
+        private double result, lastNumber;
+
+        [ObservableProperty]
+        SelectedOperator selectedOperator;
+
+        [ObservableProperty]
+        private Label resultLabel;
+
+
+
+        [RelayCommand]
+        public void CalculateAndSave()
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Sustraction:
+                        result = SimpleMath.Sustraction(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
+
+                resultLabel.Content = result.ToString();
+            }
+
+           //eakReferenceMessenger.Default.Send(new SaveMessege(renewNumbersult));
+        }
+    }
+}
